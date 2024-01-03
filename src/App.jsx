91257b1ec9bf6c78e6436ddfad645dfe5826cc40
Ian Bruce <ianbruce.braincue@gmail.com>
2024-01-03 11:27:34 -0500

    add: on Add button click handleSubmit by setting Todos array state and printing to page in list

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,56 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
+  const [newTask, setNewTask] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title: newTask,
+          completed: false
+        }
+      ]
+    })
+  }
+
+  console.log(todos)
+
   return (
     <>
       <form className="new-task-form">
         <div className="form-row">
           <label htmlFor="task">New Task</label>
-          <input type="text" id="task" />
+          <input 
+            value={newTask} 
+            onChange={e => setNewTask(e.target.value)} 
+            type="text" 
+            id="task" 
+          />
         </div>
-        <button className="btn">Add</button>
+        <button 
+        onClick={handleSubmit}
+        className="btn">Add</button>
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Task 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.map(todo => {
+          return (
+          <li key={todo.key}>
+            <label>
+              <input type="checkbox" checked={todo.completed}/>
+              {todo.title}
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+          )
+        })}
       </ul>
     </>
   )
