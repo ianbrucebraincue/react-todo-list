6e27a6d53a38b3dc9bbfca23da791839e98f23ff
Ian Bruce <ianbruce.braincue@gmail.com>
2024-01-03 11:56:30 -0500

    add: toggle todos checked and uncheck. delete todos from list. no todos when length of todos state is equal to zero.

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,27 @@ export default function App() {
         }
       ]
     })
+
+    setNewTask("")
   }
 
-  console.log(todos)
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id != id)
+    })
+  }
 
   return (
     <>
@@ -40,14 +58,21 @@ export default function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todos"}
         {todos.map(todo => {
           return (
           <li key={todo.key}>
             <label>
-              <input type="checkbox" checked={todo.completed}/>
+              <input 
+              type="checkbox" 
+              checked={todo.completed}
+              onChange={e => toggleTodo(todo.id, e.target.checked)}/>
               {todo.title}
             </label>
-            <button className="btn btn-danger">Delete</button>
+            <button 
+              onClick={() => deleteTodo(todo.id)}
+              className="btn btn-danger">Delete
+            </button>
           </li>
           )
         })}
