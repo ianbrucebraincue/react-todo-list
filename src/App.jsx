244181789edeeebfcb1888041891cd2c5c02a175
Ian Bruce <ianbruce.braincue@gmail.com>
2024-01-03 08:58:44 -0500

    add: styles file and imported into App. form with task label and input and Add button for adding new tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import "./styles.css"
 
 export default function App() {
-  return "Hi";
+  return <form className="new-task-form">
+    <div className="form-row">
+      <label htmlFor="item">New Task</label>
+      <input type="text" id="task" />
+    </div>
+    <button className="btn">Add</button>
+  </form>
 }
