65fc1dfb1d2fec409b4f7f1c0f08ffa11cc21d7a
Ian Bruce <ianbruce.braincue@gmail.com>
2024-01-03 10:31:55 -0500

    add: todo list header and list static elements. delcarative output for style check

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import "./styles.css"
 
 export default function App() {
-  return <form className="new-task-form">
-    <div className="form-row">
-      <label htmlFor="item">New Task</label>
-      <input type="text" id="task" />
-    </div>
-    <button className="btn">Add</button>
-  </form>
+  return (
+    <>
+      <form className="new-task-form">
+        <div className="form-row">
+          <label htmlFor="task">New Task</label>
+          <input type="text" id="task" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Task 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  )
 }
